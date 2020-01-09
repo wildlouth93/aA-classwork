56df060a36f0c820565ecf67eb9d382084c17a68
Willie Louth <willielouth@gmail.com>
2020-01-09 17:02:16 -0600

    Add classwork for week 22 day 4

---
+++ w22d4/bubble_sort_project/lib/bubble_sort.js
@@ -0,0 +1,27 @@
+function swap(array, idx1, idx2) {
+    let temp = array[idx1];
+    array[idx1] = array[idx2];
+    array[idx2] = temp;
+    return array;
+}
+
+function bubbleSort(array) {
+    let sorted = false;
+
+    while (!sorted) {
+        sorted = true; 
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] > array[i + 1]) {
+                sorted = false;
+                swap(array, i, i+1)
+            }
+        }
+    }
+    return array;
+}
+
+
+module.exports = {
+    bubbleSort,
+    swap
+};
